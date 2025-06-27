72492416a1bef49881566195e1037df2276085e5
migouelito <migouelito123@gmail.com>
2025-06-27 18:40:39 +0000

    Commit automatique : 2025-06-27 18:40:39

--- stageProject/settings.py
+++ stageProject/settings.py
@@ -152,9 +152,15 @@ ASGI_APPLICATION = 'stageproject.asgi.application'
 import os
 import dj_database_url
 
+
+
 DATABASES = {
-    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
+    'default': dj_database_url.config(
+        default=os.environ.get('DATABASE_URL'),
+        conn_max_age=600
+    )
 }
+
 
 
 # Password validation
