e4b4a32fc84067ca7d329981df58b7223568b338
migouelito <migouelito123@gmail.com>
2025-07-09 09:48:50 +0000

    Commit automatique : 2025-07-09 09:48:50

--- stageProject/settings.py
+++ stageProject/settings.py
@@ -42,7 +42,7 @@ INSTALLED_APPS = [
     'realtime',  # L'application que tu viens de créer
     'channels',
     'rest_framework_simplejwt',  
-    'rest_framework_simplejwt.token_blacklist', 
+    #'rest_framework_simplejwt.token_blacklist', 
     'utilisateurs',
     'capteurs',
 ]
