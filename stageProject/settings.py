88c50075ac4c9a75f49dbac98eb988e5c5e6fba6
migouelito <migouelito123@gmail.com>
2025-07-26 13:05:54 +0000

    Commit automatique : 2025-07-26 13:05:54

--- stageProject/settings.py
+++ stageProject/settings.py
@@ -43,6 +43,12 @@ INSTALLED_APPS = [
 
 ASGI_APPLICATION = 'stageProject.asgi.application'  # Remplace 'ton_projet' par le nom de ton projet
 
+
+
+WSGI_APPLICATION = 'stageProject.wsgi.application'
+
+
+
 CHANNEL_LAYERS = {
     'default': {
         'BACKEND': 'channels_redis.core.RedisChannelLayer',
